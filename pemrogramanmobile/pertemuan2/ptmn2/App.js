7e06f778635ea366fa5f042c92db11d11a4ad6d7
faiz-al123 <faizalfarzei@gmail.com>
2026-09-21 08:27:16 +0700

    today

--- pemrogramanmobile/pertemuan2/ptmn2/App.js
+++ pemrogramanmobile/pertemuan2/ptmn2/App.js
@@ -7,7 +7,7 @@ export default function App() {
       <Text>Nama: Faiz Alfaresi</Text>
       <Text>NIM: 2488010012</Text>
       <Text>Cita-cita: Data analyst</Text>
-      <Text>Rencana mencapai cita-cita seorang data analyst dengan belajar keterampilan teknis</Text>
+      <Text>Rencana mencapai cita-cita menjadi seorang data analyst dengan belajar keterampilan teknis</Text>
       <Text>keterampilan teknis untuk mengolah data serta keterampilan non-teknis untuk memahami kebutuhan bisnis</Text>
       <StatusBar style="auto" />
     </View>
